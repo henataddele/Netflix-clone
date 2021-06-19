28f416b6f50272c8f1d47a93183ffadae42cbe09
henataddele <81271964+henataddele@users.noreply.github.com>
2021-06-18 21:55:05 -0400

    Add files via upload

---
+++ build/precache-manifest.92bc681fe21a97b348ecab11301db34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fc6a5475b3e71ce89c05aa901c1f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d33a67c663fc05beb7b",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d33a67c663fc05beb7b",
+    "url": "/static/js/main.ea79a1d2.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
